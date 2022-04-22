c8ddc18a2da6e00d0464c7f8cf56e6d412ffaa12
dayhele <dayhelepereira@gmail.com>
2022-04-21 22:35:16 -0300

    creating header

--- podcastr/src/components/Header/index.tsx
+++ podcastr/src/components/Header/index.tsx
@@ -1,11 +1,18 @@
-import Image from "next-image";
+import format from "date-fns/format";
+import ptBR from "date-fns/locale/pt-BR";
+import styles from "./styles.module.scss";
+import Image from "next/image";
 
 export default function Header() {
+  const currentDate = format(new Date(), "EEEEEE, d MMMM", {
+    locale: ptBR,
+  });
+
   return (
-    <header>
+    <header className={styles.headerContainer}>
       <Image src="/logo.svg" alt="Podcastr" width={162} height={40} />
-      <p></p>
-      <span></span>
+      <p>O melhor para você ouvir, sempre</p>
+      <span>{currentDate}</span>
     </header>
   );
 }
