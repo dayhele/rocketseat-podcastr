0265082ae819f455cdee38c680d591c30546b51f
dayhele <dayhelepereira@gmail.com>
2022-04-21 22:41:57 -0300

    creating player

---
+++ podcastr/src/components/Player/index.tsx
@@ -0,0 +1,6 @@
+import styles from "./styles.module.scss";
+import Image from "next/image";
+
+export default function Player() {
+  return <div></div>;
+}
--- podcastr/src/pages/_app.tsx
+++ podcastr/src/pages/_app.tsx
@@ -1,11 +1,18 @@
-import Header from "../components/Header";
 import "../styles/global.scss";
+
+import Header from "../components/Header";
+import Player from "../components/Player";
+
+import styles from "../styles/app.module.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
-      <Header />
-      <Component {...pageProps} />;
+    <div className={styles.appWrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />;
+      </main>
+      <Player />
     </div>
   );
 }
